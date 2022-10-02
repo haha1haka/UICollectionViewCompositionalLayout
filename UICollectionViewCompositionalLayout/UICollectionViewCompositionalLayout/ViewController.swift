8c682679110a1a4c6f894d57b8af05c258c80743
haha1haka <haha1haka@icloud.com>
2022-10-02 17:32:23 +0900

    nested group

--- UICollectionViewCompositionalLayout/UICollectionViewCompositionalLayout/ViewController.swift
+++ UICollectionViewCompositionalLayout/UICollectionViewCompositionalLayout/ViewController.swift
@@ -23,12 +23,13 @@ extension ViewController {
         super.viewDidLoad()
         //gridCollectionViewLayout()
         //twoColumnGridCollectionViewLayout()
-        
+        nestedGroupCollectionViewLayout()
         configureCollectionViewDataSource()
         applySnapShot()
     }
 }
 extension ViewController {
+    
     func gridCollectionViewLayout() {
         let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.2),
                                               heightDimension: .fractionalHeight(1.0))
@@ -41,6 +42,7 @@ extension ViewController {
         let section = NSCollectionLayoutSection(group: group)
         collectionView.collectionViewLayout = UICollectionViewCompositionalLayout(section: section)
     }
+    
     func twoColumnGridCollectionViewLayout() {
         let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1.0))
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
@@ -50,6 +52,33 @@ extension ViewController {
                                                        subitem: item,
                                                        count: 2)
         let section = NSCollectionLayoutSection(group: group)
+        collectionView.collectionViewLayout = UICollectionViewCompositionalLayout(section: section)
+    }
+    
+    func nestedGroupCollectionViewLayout() {
+        let leftItemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(2/3),
+                                                  heightDimension: .fractionalHeight(1.0))
+        let leftItem = NSCollectionLayoutItem(layoutSize: leftItemSize)
+        leftItem.contentInsets = NSDirectionalEdgeInsets(top: 3, leading: 3, bottom: 3, trailing: 3)
+        let rightItemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                   heightDimension: .fractionalHeight(0.5))
+        let rightItem = NSCollectionLayoutItem(layoutSize: rightItemSize)
+        
+        
+        rightItem.contentInsets = NSDirectionalEdgeInsets(top: 3, leading: 3, bottom: 3, trailing: 3)
+        let rightGroup = NSCollectionLayoutGroup.vertical(layoutSize:
+                                                            NSCollectionLayoutSize(
+                                                                widthDimension: .fractionalWidth(1/3),
+                                                                heightDimension: .fractionalHeight(1.0)),
+                                                          subitem: rightItem,
+                                                          count: 2)
+        
+        
+        let toalGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                   heightDimension: .fractionalWidth(4/9))
+        let totalGroup = NSCollectionLayoutGroup.horizontal(layoutSize: toalGroupSize,
+                                                            subitems: [leftItem, rightGroup])
+        let section = NSCollectionLayoutSection(group: totalGroup)
         collectionView.collectionViewLayout = UICollectionViewCompositionalLayout(section: section)
     }
 
